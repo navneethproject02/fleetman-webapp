feb1e462909a935dd137abc72ed8430fa2c812da
navneethproject02 <navneethproject02@gmail.com>
2017-04-12 14:49:54 -0400

    Update FleetmanApplicationTests.java

--- src/test/java/com/virtualpairprogrammers/FleetmanApplicationTests.java
+++ src/test/java/com/virtualpairprogrammers/FleetmanApplicationTests.java
@@ -1,4 +1,4 @@
-package com.virtualpairprogrammers;
+/*package com.virtualpairprogrammers;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,4 +15,4 @@ public class FleetmanApplicationTests {
 	public void contextLoads() {
 	}
 
-}
+}*/
